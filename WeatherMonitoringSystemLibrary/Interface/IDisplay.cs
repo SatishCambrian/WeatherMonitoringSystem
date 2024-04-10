d0d30f9b197d6779aed26802ab8fecd468e15a7c
SatishCambrian <SatishCambrian@users.noreply.github.com>
2024-04-10 17:57:26 -0400

    Interface IDisplay Created.

---
+++ WeatherMonitoringSystemLibrary/Interface/IDisplay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMonitoringSystemLibrary.Interface
+{
+    internal interface IDisplay
+    {
+        /// <summary>
+        /// Interface for all Display Elements
+        /// </summary>
+        void display();
+
+    }
+}
