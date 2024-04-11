b2f4b197d5bd28e9ca4457a1b0c0a82509cf4bcd
SatishCambrian <SatishCambrian@users.noreply.github.com>
2024-04-10 21:19:51 -0400

    ForecastDisplay added

---
+++ WeatherMonitoringSystemLibrary/ForecastDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherMonitoringSystemLibrary.Interfaces;
+
+namespace WeatherMonitoringSystemLibrary
+{
+    public class ForecastDisplay: IDisplay
+    {
+        private readonly IWeatherDataSubject _weatherDataSubject;
+        public ForecastDisplay(IWeatherDataSubject weatherDataSubject)
+        {
+            this._weatherDataSubject = weatherDataSubject;
+            weatherDataSubject.RegisterObserver(this);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Weather is Rainy.")
+        }
+    }
+}
